6ca3ee778f5d298e6a5cce33373e576a51c47262
Juan Suarez <juan.suarez1@utp.edu.co>
2026-04-18 11:05:31 -0500

    clase 17/04/2026

--- index.js
+++ index.js
@@ -2,23 +2,55 @@ const express = require('express')
 const servidor = express()
 const port = 3000
 
-// servidor.get('/', (req, res) => {
-//   console.log('Recibí una solicitud GET en la raíz')  
-//   res.send('Hola mundo!')
-// })
+/////////////middlewares///////////////
+function notificarPeticiones(peticion, respuesta, siguiente){
+  console.log("Está llegando una peticion")
+  siguiente()
+}
 
-// servidor.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+servidor.use(notificarPeticiones)
+servidor.use(express.json())
+
+///// funciones GET, POST , DELETe, PUT /////////////
+
+estudiantes = ["pepe", "juan", "andrea", "sara"]
+
+function getRedes(peticion, respuesta) {
+  headers = peticion.headers
+  body = peticion.body
+  // console.log(headers)
+  console.log(body)
+  respuesta.status(200).json(headers)
+}
+
+function postParaRedes(peticion, respuesta) {
+  console.log("peticion POST en la ruta redes")
+  respuesta.send("Peticion POST en ruta redes recibida!!")
+}
+
+function getEstudiantes(peticion, respuesta) {
+  console.log("peticion POST en la ruta redes")
+  respuesta.json(estudiantes)
+}
+
+function postEstudiantes(peticion, respuesta){
+  body = peticion.body
+  nombre = body.nombre
+
+  estudiantes.push(nombre)
+  respuesta.status(201).send("El estudiante fue añadido a la lista")
+}
+
+
+servidor.get("/redes", getRedes)
+servidor.get("/estudiantes", getEstudiantes)
+servidor.post("/redes", postParaRedes)
+servidor.post("/estudiantes", postEstudiantes)
+
+
+
+/////     inicializacion     ///////////////////
 function iniciarServidor() {
-  console.log('Servidor escuchando')  
+  console.log('Servidor escuchando')
 }
-
-function getRedes(peticion, respuesta){
-    console.log("esta es la ruta redes!!!")
-    respuesta.send("Estas en la ruta redes")
-}
-
-servidor.get("/redes",getRedes)
-
 servidor.listen(port, iniciarServidor)
